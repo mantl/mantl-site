456115cf96c33fa26577897435737845b861eada
Ulises Soriano Palao <ulisessorianopalao@gmail.com>
2016-06-15 21:48:37 +0200

    Added large header styles

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,27 +1,35 @@
 <header>
-            
-    <button class="menu-toggle toggle-button--htx"><span>Toggle Menu</span></button>
 
-    <a class="logo" href="{{ url('/') }}">Home</a>
+	<div class="row">
+			
+		<div class="columns small-12">
+			
+			<button class="menu-toggle toggle-button--htx show-for-small-only"><span>Toggle Menu</span></button>
+
+		    <a class="logo" href="{{ url('/') }}">Home</a>
+			
+			<nav class="menu">
+
+			    <ul class="main">
+			                        
+			        <li><a href="{{ url('/features') }}" @if($pageID == 1) class="active" @endif>Features</a></li>
+			        
+			        <li><a href="{{ url('/technologies') }}" @if($pageID == 2) class="active" @endif>Technologies</a></li>
+			        
+			        <li><a href="{{ url('/resources') }}" @if($pageID == 3) class="active" @endif>Resources</a></li>
+			        
+			        <li><a href="{{ url('/faq') }}" @if($pageID == 4) class="active" @endif>FAQ</a></li>
+			        
+			        <li><a href="{{ url('/download') }}" @if($pageID == 5) class="active" @endif>Download</a></li>
+			        
+			    </ul>
+
+			    @include('components.icons-menu')
+
+			</nav>
 	
-	<nav class="menu">
-
-	    <ul class="main">
-	                        
-	        <li><a href="{{ url('/features') }}" @if($pageID == 1) class="active" @endif>Features</a></li>
-	        
-	        <li><a href="{{ url('/technologies') }}" @if($pageID == 2) class="active" @endif>Technologies</a></li>
-	        
-	        <li><a href="{{ url('/resources') }}" @if($pageID == 3) class="active" @endif>Resources</a></li>
-	        
-	        <li><a href="{{ url('/faq') }}" @if($pageID == 4) class="active" @endif>FAQ</a></li>
-	        
-	        <li><a href="{{ url('/download') }}" @if($pageID == 5) class="active" @endif>Download</a></li>
-	        
-	    </ul>
-
-	    @include('components.icons-menu')
-
-	</nav>
+		</div>
+	
+	</div>
 
 </header>
